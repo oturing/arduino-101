986e09a704c1358de8e10024df61c9cbf1a98a82
Luciano Ramalho <luciano@ramalho.org>
2013-09-13 10:56:28 -0300

    mais slides sobre display 7 segmentos; refatorao no sketch (ainda nao esta 100%)

--- sketches/Display3digits/Display3digits.ino
+++ sketches/Display3digits/Display3digits.ino
@@ -25,45 +25,87 @@ int algarismos [][7] = {
 };
 
 int NUM_ALGARISMOS = 10;
-int numero;
-int qt_digitos;
-unsigned long atraso = 500;
-unsigned long atualizacao;
+int numero = 850; // numero a ser exibido
+int qt_digitos_ativos;   // quantidade de digitos em uso
+unsigned long INTERVALO = 100; // intervalo entre incrementos do numero
+unsigned long atualizacao; // momento da atualizacao mais recente
 
 
 void setup() {                
   int i;
+  // configurar e desativar todos os digitos
   for(i=0; i<NUM_DIGITOS; i++) {
     pinMode(pinos_digitos[i], OUTPUT);
     digitalWrite(pinos_digitos[i], LOW);
   }
+  // configurar e desativar todos os segmentos
   for(i=0; i<NUM_SEGMENTOS; i++) {
     pinMode(pinos_segmentos[i], OUTPUT);
+    // 5V no catodo zera a corrente no segmento
     digitalWrite(pinos_segmentos[i], HIGH);
   }
   atualizacao = millis();
-  numero = 0;
+}
+
+int contar_digitos(int n) {
+  // calcular quantidade de digitos no numero atual
+  int digitos = 0;
+  while (n > 0) {
+    digitos++;
+    n /= 10;
+  }
+  return digitos;
+}
+
+void configurar_segmentos(int algarismo) {
+  // usar algarismo -1 para desativar todos os segmentos
+  for (int s=0; s<NUM_SEGMENTOS; s++) {
+    if (algarismo >= 0 && algarismos[algarismo][s]) {
+      // ativar segmento: GND no catodo
+      digitalWrite(pinos_segmentos[s], LOW);
+    } else {
+      // desativar segmento: +5V no catodo
+      digitalWrite(pinos_segmentos[s], HIGH);
+    }
+  }
 }
 
 void loop() {
-  qt_digitos = (int)(log(numero) / log(10)) + 1;
-  for (int d=qt_digitos-1; d>=0; d--) {
-    int resto = (numero / (int)pow(10, d)) % 10;
+  // percorrer digitos a partir do mais significativo
+  // for (int d=qt_digitos-1; d>=0; d--) {
+  // percorrer digitos a partir do menos significativo
+  for (int d=0; d<qt_digitos_ativos; d++) {
+    // ativar digito: +5V no anodo
     digitalWrite(pinos_digitos[d], HIGH);
-    for (int s=0; s<NUM_SEGMENTOS; s++) {
-      if (algarismos[resto][s]) {
-        digitalWrite(pinos_segmentos[s], LOW);
-      }
-    }
+    int resto = (numero / (int)pow(10, d)) % 10;
+    configurar_segmentos(resto);
+    // empiricamente verifiquei que este pequeno atraso 
+    // maximiza o brilho e nao produz flicker visivel
     delay(3);
+    // desativar todos os segmentos
     for (int s=0; s<NUM_SEGMENTOS; s++) {
       digitalWrite(pinos_segmentos[s], HIGH);
-    } 
+    }
+   // desativar digito 
     digitalWrite(pinos_digitos[d], LOW);
   }
   unsigned long agora = millis();
-  if ((agora - atualizacao) > atraso) {
+  int qt_digitos;
+  if ((agora - atualizacao) > INTERVALO) {
      numero = numero < (int)(pow(10, NUM_DIGITOS)-1) ? numero + 1 : 0;
+     qt_digitos = contar_digitos(numero);
+     if (qt_digitos != qt_digitos_ativos) {
+       for (int d = 0; d < NUM_DIGITOS; d++) {
+         if (d < qt_digitos) {
+           // escrever zeros
+           configurar_segmentos(0);      
+         } else {
+           // apagar digito
+           configurar_segmentos(-1);
+         }
+       }
+       qt_digitos_ativos = qt_digitos;
+     }
      atualizacao = agora;
   }
 }
